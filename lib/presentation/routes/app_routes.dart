4c6131654a2a5be4108a98ae510e619689d662c7
abdulrahmanAshraf272 <abdulrahmanashraf272@gmail.com>
2025-02-11 22:33:46 +0200

    built  app structure

---
+++ lib/presentation/routes/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/presentation/routes/route_names.dart';
+import 'package:movies_app/presentation/screens/home/home_screen.dart';
+
+class AppRoutes {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteNames.home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+    }
+  }
+}
